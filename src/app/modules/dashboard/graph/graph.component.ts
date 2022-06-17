d7b941efdda2ce0cc28be3d42faa414e90f389be
DasiMq <dasikarans.19@uom.lk>
2022-06-17 19:48:08 +0530

    Dashboard graph re-designed

--- src/app/modules/dashboard/graph/graph.component.ts
+++ src/app/modules/dashboard/graph/graph.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef,Input, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import * as Chart from 'chart.js';
 
 @Component({
@@ -7,67 +7,76 @@ import * as Chart from 'chart.js';
   styleUrls: ['./graph.component.css']
 })
 export class GraphComponent implements AfterViewInit {
-  @Input() graphData:any;
+  @Input() graphData: any;
   @ViewChild('lineCanvas') lineCanvas!: ElementRef;
   lineChart: any;
-constructor() { }
-ngAfterViewInit(): void {
+  constructor() { }
+  ngAfterViewInit(): void {
     this.lineChartMethod();
   }
-lineChartMethod() {
-  var gradientChartOptionsConfigurationWithTooltipGreen: any = {
-    maintainAspectRatio: false,
-    legend: {
-      display: false
-    },
+  lineChartMethod() {
+    var gradientChartOptionsConfigurationWithTooltipGreen: any = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
 
-    tooltips: {
-      backgroundColor: 'rgba(255,255,255, 0.5)',
-      titleFontColor: '#333',
-      bodyFontColor: '#666',
-      bodySpacing: 4,
-      xPadding: 12,
-      mode: "nearest",
-      intersect: 0,
-      position: "nearest"
-    },
-    responsive: true,
-    scales: {
-      yAxes: [{
-        barPercentage: 1.6,
-        gridLines: {
-          drawBorder: false,
-          color: 'rgba(52, 122, 226, 0.0)',
-          zeroLineColor: "transparent",
-        },
-        ticks: {
-          suggestedMin: 50,
-          suggestedMax: 125,
-          padding: 20,
-          fontColor: "#9e9e9e"
-        }
-      }],
+      tooltips: {
+        backgroundColor: 'rgba(255,255,255, 0.5)',
+        titleFontColor: '#333',
+        bodyFontColor: '#666',
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+      },
 
-      xAxes: [{
-        barPercentage: 1.6,
-        gridLines: {
-          drawBorder: false,
-          color: 'rgba(52, 122, 226, 0.1)',
-          zeroLineColor: "transparent",
-        },
-        ticks: {
-          padding: 20,
-          fontColor: "#9e9e9e"
-        }
-      }]
-    }
-  };
+      responsive: true,
+      scales: {
+        yAxes: [{
+          barPercentage: 1.6,
+          gridLines: {
+            drawBorder: false,
+            color: 'rgba(52, 122, 226, 0.0)',
+            zeroLineColor: "transparent",
+          },
+          ticks: {
+            suggestedMin: 50,
+            suggestedMax: 120,
+            padding: 20,
+            fontColor: "#292C38",
+            fontFamily: "Poppins",
+            fontWeight: 600,
+            fontSize: 12
+          }
+        }],
 
-  var ctx = this.lineCanvas.nativeElement.getContext('2d');
-  var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
-  gradientStroke.addColorStop(1, 'rgba(52, 122, 226, 0.2)');
-    gradientStroke.addColorStop(0.4, 'rgba(52, 122, 226,0.0 )');
-    gradientStroke.addColorStop(0, 'rgba(52, 122, 226, 0)'); //red colors
+        xAxes: [{
+          barPercentage: 1.6,
+          gridLines: {
+            drawBorder: false,
+            color: 'rgba(52, 122, 226, 0.1)',
+            zeroLineColor: "transparent",
+          },
+          ticks: {
+            padding: 20,
+            fontColor: "#292C38",
+            fontFamily: "Poppins",
+            fontSize: 13
+          },
+          test: {
+            fontWeight: 900,
+          }
+        }]
+      }
+    };
+
+    var ctx = this.lineCanvas.nativeElement.getContext('2d');
+    var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+    gradientStroke.addColorStop(1, 'rgba(52, 122, 226, 1)');
+    gradientStroke.addColorStop(0.1, 'rgba(52, 122, 226,0 )');
+    gradientStroke.addColorStop(0.15, 'rgba(52, 122, 226, 0)'); //red colors
 
 
 
@@ -81,7 +90,7 @@ lineChartMethod() {
             fill: true,
             backgroundColor: gradientStroke,
             borderColor: '#347ae2',
-            borderWidth: 2,
+            borderWidth: 0.05,
             borderDash: [],
             borderDashOffset: 0.0,
             pointBackgroundColor: '#347ae2',
@@ -90,8 +99,9 @@ lineChartMethod() {
             pointBorderWidth: 20,
             pointHoverRadius: 4,
             pointHoverBorderWidth: 15,
-            pointRadius: 4,
+            pointRadius: 0,
             data: this.graphData.data,
+            
           }
         ]
       },
