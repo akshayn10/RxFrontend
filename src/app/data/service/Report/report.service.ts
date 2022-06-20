0aa81e70eaa432a6e44f2e6796116b4953bce970
akshayn10 <thiruakshayan98@gmail.com>
2022-06-20 14:20:29 +0530

    Report changes

--- src/app/data/service/Report/report.service.ts
+++ src/app/data/service/Report/report.service.ts
@@ -7,12 +7,34 @@ import { Stats } from '../../schema/Stats';
   providedIn: 'root'
 })
 export class ReportService {
-  readonly baseURL = 'https://localhost:44352/api/report';
+  readonly baseURL = 'https://localhost:44352/api/report/';
 
   constructor(private http:HttpClient) { }
 
   getSubscriptionStats():Observable<Stats[]>{
-    return this.http.get<Stats[]>(this.baseURL);
+    return this.http.get<Stats[]>(this.baseURL+"sub-summary");
+  }
+  getActivationsStats():Observable<Stats[]>{
+    return this.http.get<Stats[]>(this.baseURL+"sub-activation");
+  }
+  getUpgradesStats():Observable<Stats[]>{
+    return this.http.get<Stats[]>(this.baseURL+"upgrade");
+  }
+  getDowngradeStats():Observable<Stats[]>{
+    return this.http.get<Stats[]>(this.baseURL+"downgrade");
+  }
+  getUnsubscriptionStats():Observable<Stats[]>{
+    return this.http.get<Stats[]>(this.baseURL+"unsubscribe");
 
   }
+  getSalesByPlanStats():Observable<Stats[]>{
+    return this.http.get<Stats[]>(this.baseURL+"sales-by-plan");
+  }
+  getSalesByAddOnStats():Observable<Stats[]>{
+    return this.http.get<Stats[]>(this.baseURL+"sales-by-addOn");
+  }
+  getRevenueStats():Observable<Stats[]>{
+    return this.http.get<Stats[]>(this.baseURL+"revenue");
+  }
+
 }
