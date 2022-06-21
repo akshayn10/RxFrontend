78fe59a3e72c8d843dfc95853f4ae9f6e50c7ad7
akshayn10 <thiruakshayan98@gmail.com>
2022-06-21 22:21:58 +0530

    Changes

--- src/app/data/service/auth/user.service.ts
+++ src/app/data/service/auth/user.service.ts
@@ -1,14 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { OrganizationUser } from '../../schema/Organization/organizationUser';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
   private userBaseApiUrl = environment.baseApiUrl+'user/';
 
   constructor(private http:HttpClient) { }
+
+  getUsersForOrganization(organizationId:string):Observable<OrganizationUser[]>{
+    return this.http.get<OrganizationUser[]>(this.userBaseApiUrl+'/organization-users/'+organizationId);
+  }
+  addUserToOrganization(value: any) {
+    return this.http.post(this.userBaseApiUrl+"add-user", value,{
+      responseType:'text'
+    });
+  }
 
 }
